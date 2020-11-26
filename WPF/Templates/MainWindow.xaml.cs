fbea409f2d7587edf567318b213dd0365dcc9c51
mammadzadaa <mammedzadea@itstep.edu.az>
2020-11-26 14:08:40 +0400

    lesson_26_11_20

---
+++ WPF/Templates/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Templates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            myImage.Margin = new Thickness(10);
+            Uri uri = new Uri(@"C:\Users\Aqil\Downloads\space.jpg",UriKind.Absolute);
+            ImageSource imageSource = new BitmapImage(uri);
+            myImage.Source = imageSource;
+        }
+    }
+}
