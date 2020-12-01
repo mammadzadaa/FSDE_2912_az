5f4e5323b1885999c0bf4e9aa7beb3de5f5047a4
mammadzadaa <mammedzadea@itstep.edu.az>
2020-12-01 21:54:15 +0400

    lesson_1_12_20

---
+++ WPF/ToDoListMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using ToDoListMVVM.Models;
+
+namespace ToDoListMVVM.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string taskName;
+        private string taskDescription;
+        private ObservableCollection<MyTask> myTasks;
+        private bool taskIsDone;
+        private DateTime taskDeadline = DateTime.Now;
+        private CommandBase addTaskCommand;
+
+        public string TaskName { get => taskName; set => OnChanged(out taskName, value); }
+        public string TaskDescription { get => taskDescription; set => OnChanged(out taskDescription, value); }
+        public bool TaskIsDone { get => taskIsDone; set => OnChanged(out taskIsDone, value); }
+        public DateTime TaskDeadline { get => taskDeadline; set => OnChanged(out taskDeadline, value); }
+        public ObservableCollection<MyTask> MyTasks { get => myTasks; set => OnChanged(out myTasks, value); }
+        public MyTask SelectedItem { get; set; }
+
+        public CommandBase AddTaskCommand => addTaskCommand ?? (addTaskCommand = new CommandBase(x => AddTask()));
+       
+        //{
+        //    get
+        //    {
+        //        return addTaskCommand ?? (addTaskCommand = new CommandBase(x => AddTask()));
+
+        //        //if (addTaskCommand == null)
+        //        //{
+        //        //    addTaskCommand = new CommandBase(x => AddTask());
+        //        //}
+        //        //return addTaskCommand;
+        //    }
+        //} 
+        public CommandBase RemoveTaskCommand { get; set; }
+
+        public MainViewModel()
+        {
+           
+            RemoveTaskCommand = new CommandBase(x =>
+            {
+                RemoveTask();
+            });
+            myTasks = new ObservableCollection<MyTask>();
+        }
+
+        public void AddTask()
+        {
+            MyTasks.Add(new MyTask()
+            {
+                Name = TaskName,
+                Description = TaskDescription,
+                Deadline = TaskDeadline,
+                IsDone = TaskIsDone
+            });
+            Clear();
+        }
+
+        public void RemoveTask()
+        {
+            if (SelectedItem != null)
+                MyTasks.Remove(SelectedItem);
+        }
+
+        private void Clear()
+        {
+            TaskName = string.Empty;
+            TaskDescription = string.Empty;
+            TaskDeadline = DateTime.Now;
+            TaskIsDone = false;
+        }
+    }
+
+
+    public class CommandBase : ICommand
+    {
+        private Action<object> action;
+        
+        public CommandBase(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            action?.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+    }
+
+
+
+    //public class AddTaskCommand : ICommand
+    //{
+    //    public MainViewModel ViewModel { get; set; }
+    //    public AddTaskCommand(MainViewModel viewModel)
+    //    {
+    //        ViewModel = viewModel;
+    //    }
+    //    public void Execute(object parameter)
+    //    {
+    //        ViewModel.AddTask();
+    //    }
+
+    //    public event EventHandler CanExecuteChanged;
+
+    //    public bool CanExecute(object parameter) => true;
+    //}
+
+    //public class RemoveTaskCommand : ICommand
+    //{
+    //    public MainViewModel ViewModel { get; set; }
+    //    public RemoveTaskCommand(MainViewModel viewModel)
+    //    {
+    //        ViewModel = viewModel;
+    //    }
+    //    public void Execute(object parameter)
+    //    {
+    //        ViewModel.RemoveTask();
+    //    }
+
+    //    public event EventHandler CanExecuteChanged;
+
+    //    public bool CanExecute(object parameter) => true;
+    //}
+}
---
+++ WPF/ToDoListMVVM/Views/MainWindowView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ToDoListMVVM.Models;
+using ToDoListMVVM.ViewModels;
+
+namespace ToDoListMVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    { 
+
+        public MainWindowView()
+        {
+            InitializeComponent();            
+            DataContext = new MainViewModel(); 
+        }
+    }
+}
