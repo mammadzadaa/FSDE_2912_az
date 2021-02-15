45ff95c5415419552ac9950fd0d39c6f40433e9c
mammadzadaa <mammedzadea@itstep.edu.az>
2021-02-15 22:02:34 +0400

    lesson_15_02_2021

---
+++ ADO.NET/AdoIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace AdoIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = @"localhost\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "StepITAcademy";
+            connectionStringBuilder.IntegratedSecurity = true;
+
+            Console.WriteLine(connectionStringBuilder.ToString());
+            using (var connection = new SqlConnection(connectionStringBuilder.ToString()))
+            {
+                connection.Open();
+                Console.WriteLine("Connection is open!\n");                
+                //var querry = "INSERT INTO Students VALUES('Zabil','Hajiyev','2001-04-29',1000,'zabil001@mail.ru')";
+                var querry = "SELECT * FROM Students";
+                var command = new SqlCommand(querry, connection);
+                //command.ExecuteNonQuery();
+                var result = command.ExecuteReader();
+                while (result.Read())
+                {
+                    Console.Write((int)result[0]);
+                    Console.WriteLine(" " + (string)result[1]);
+                }
+            }
+            Console.WriteLine("Connection closed!\n");
+
+
+        }
+    }
+}
