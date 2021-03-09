6b5f3ad5e63470a07dc1964d6cd2dd61e0d54254
mammadzadaa <mammedzadea@itstep.edu.az>
2021-03-09 22:01:51 +0400

    lesson_09_03_21

--- ADO.NET/Lesson_08_03_21/Program.cs
+++ ADO.NET/Lesson_08_03_21/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lesson_08_03_21.Models;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Lesson_08_03_21
 {
@@ -12,16 +14,30 @@ namespace Lesson_08_03_21
     {
         static void Main(string[] args)
         {
+            var _conn_str = @"data source=localhost\SQLEXPRESS;initial catalog=University;integrated security=True;MultipleActiveResultSets=True";
+            Console.ReadKey();
+            using (var _conn = new SqlConnection(_conn_str))
+            {
+                _conn.Open();
+                var s = _conn.Query<Student>("SELECT * FROM Students").ToList();
+                Console.WriteLine(s[0].Group.Name);
+                s.ForEach(Console.WriteLine);
+            }
+            Console.ReadKey();
+
+
             using (UniversityDB db = new UniversityDB())
             {
-                var g = db.Groups.FirstOrDefault();
-                //var g = db.Groups.Include("Students").FirstOrDefault();
-                //db.Entry(g).Collection(x => x.Teachers).Load();
+                 db.Students.ToList().ForEach(Console.WriteLine);
+                
+                //var g = db.Groups.FirstOrDefault();
+                ////var g = db.Groups.Include("Students").FirstOrDefault();
+                ////db.Entry(g).Collection(x => x.Teachers).Load();
 
-                //Console.WriteLine(g.Name);
-                g.Students.ForEach(Console.WriteLine);
-                g.Teachers.ForEach(Console.WriteLine);
-                db.Entry(g).State = System.Data.Entity.EntityState.Modified;
+                ////Console.WriteLine(g.Name);
+                //g.Students.ForEach(Console.WriteLine);
+                //g.Teachers.ForEach(Console.WriteLine);
+                //db.Entry(g).State = System.Data.Entity.EntityState.Modified;
                 //db.Database.ExecuteSqlCommand("");
 
                 //foreach (var teacher in g.Teachers)
@@ -34,9 +50,7 @@ namespace Lesson_08_03_21
                 //    Console.WriteLine(student.Name);
                 //    Console.WriteLine(student.Group.Name);
                 //}
+                }
             }
-
-
         }
-    }
 }
