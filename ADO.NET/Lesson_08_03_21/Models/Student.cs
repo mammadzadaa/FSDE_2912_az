9d0ee53098a2a2d2be81ed1214bcd70cf8e929c6
mammadzadaa <mammedzadea@itstep.edu.az>
2021-03-12 21:52:38 +0400

    lesson_12_03_21

--- ADO.NET/Lesson_08_03_21/Models/Student.cs
+++ ADO.NET/Lesson_08_03_21/Models/Student.cs
@@ -12,6 +12,7 @@ namespace Lesson_08_03_21.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public virtual Group Group { get; set; }
+
         public override string ToString()
         {
             return Name + " " + Surname;
--- ADO.NET/Lesson_08_03_21/Program.cs
+++ ADO.NET/Lesson_08_03_21/Program.cs
@@ -15,41 +15,75 @@ namespace Lesson_08_03_21
         static void Main(string[] args)
         {
             var _conn_str = @"data source=localhost\SQLEXPRESS;initial catalog=University;integrated security=True;MultipleActiveResultSets=True";
-            Console.ReadKey();
+            //Console.ReadKey();
             using (var _conn = new SqlConnection(_conn_str))
             {
                 _conn.Open();
-                var newSql = @"SELECT * FROM Students JOIN Groups ON Group_Id = Students.Group_Id";
-                var oldSql = @"SELECT * FROM Students";
-                var gd = new Dictionary<int, Group>();
-                var sd = new Dictionary<int, Student>();
-                var st = _conn.Query<Student,Group,Student>(
-                    newSql, (s,g) =>
-                    {
-                        Group gTemp;
-                        if (!gd.ContainsKey(g.Id))
-                        {
-                            gTemp = g;
-                            gd.Add(g.Id, g);
-                        }
-                        else
-                        {
-                            gTemp = gd[g.Id];
-                        }
-                            s.Group = gTemp;
-                        if(!sd.ContainsKey(s.Id))
-                        {
-                            sd.Add(s.Id, s);                            
-                            return s; 
-                        }
-                        else
-                        {
-                            return sd[s.Id];
-                        }
-                    }).Distinct().ToList();
+                var s = _conn.Query<Student>("Select * FROM Students");
+                var tempStudent = s.First(x=>x.Name=="Muradxan");
+                var querry = @"Update Students Set [Name]=@Name where Id = @Id";
+                _conn.Execute(querry, new { Name = "BurdaAdDeyisdi", Id = tempStudent.Id });
 
-                Console.WriteLine(st[0].Group.Name);
-                st.ForEach(Console.WriteLine);
+                //var querry = "INSERT INTO Groups VALUES (@Name)";
+               //_conn.Execute(querry,group);
+
+               // var group = _conn.Query<Group>("SELECT * FROM Groups");
+               // var student = new Student() { Name="Muradxan", Surname="Novruzov", Group=group.FirstOrDefault() 
+                // var querry = "INSERT INTO Students Values(@Name,@Surname,@Id)";
+                //  _conn.Execute(querry,new{student.Name, student.Surname, student.Group.Id });
+
+
+
+                //    var querry = @"SELECT * FROM Students ORDER BY Id; SELECT * FROM Groups;SELECT Group_Id FROM Students ORDER BY Id;";
+                //    var st = _conn.QueryMultiple(querry);
+                //    var s = st.Read<Student>().ToList();
+                //    var g = st.Read<Group>().ToList();
+                //    var gId = st.Read<int>().ToList();
+
+                //    for (int i = 0; i < s.Count; i++)
+                //    {
+                //        foreach (var group in g)
+                //        {
+                //            if (gId[i]== group.Id)
+                //            {
+                //                s[i].Group = group;
+                //            }
+                //        }
+                //    }
+                //    foreach (var student in s)
+                //    {
+                //        Console.WriteLine(student + student.Group.Name);
+                //    }
+                //var newSql = @"SELECT * FROM Students JOIN Groups ON Group_Id = Students.Group_Id";
+                //var oldSql = @"SELECT * FROM Students";
+                //var gd = new Dictionary<int, Group>();
+                //var sd = new Dictionary<int, Student>();
+                //var st = _conn.Query<Student,Group,Student>(
+                //    newSql, (s,g) =>
+                //    {
+                //        Group gTemp;
+                //        if (!gd.ContainsKey(g.Id))
+                //        {
+                //            gTemp = g;
+                //            gd.Add(g.Id, g);
+                //        }
+                //        else
+                //        {
+                //            gTemp = gd[g.Id];
+                //        }
+                //            s.Group = gTemp;
+                //        if(!sd.ContainsKey(s.Id))
+                //        {
+                //            sd.Add(s.Id, s);                            
+                //            return s; 
+                //        }
+                //        else
+                //        {
+                //            return sd[s.Id];
+                //        }
+                //    }).Distinct().ToList();
+                //Console.WriteLine(st[0].Group.Name);
+                //st.ForEach(Console.WriteLine);
             }
             Console.ReadKey();
 
