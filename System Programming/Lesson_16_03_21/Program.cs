bcb884ab73f861c24ff1b3668f31c55edbe84a46
mammadzadaa <mammedzadea@itstep.edu.az>
2021-03-16 21:49:33 +0400

    lesson_16_03_21

---
+++ System Programming/Lesson_16_03_21/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson_16_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach (var a in args)
+            //{
+            //    Console.Write(a + " ");
+            //    Console.WriteLine(a.Length);
+            //}
+            //var proc = new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo()
+            //    {
+            //        FileName = "chrome",
+            //        Arguments = "fb.com",
+
+            //    }
+            //};
+
+            //proc.Start();
+            //Console.ReadKey();
+            //PInvoke.Kernel32.TerminateProcess(proc.Handle,0);
+            while (true)
+            {
+                foreach (var p in Process.GetProcessesByName("msedge"))
+                {
+                    Console.WriteLine(p.ProcessName);
+                    p.Kill();
+                    // p.Exited += (s, e) => { Console.WriteLine($"{p.Id} Killed "); };
+                }
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+
+    }
+}
