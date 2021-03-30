6e001c874a002484166bdaada42d4b4ed4a06925
mammadzadaa <mammedzadea@itstep.edu.az>
2021-03-30 21:50:20 +0400

    Lesson_30_03_21

---
+++ System Programming/Lesson_30_03_21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson_30_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var t = new Timer((x) => { Console.WriteLine($"Time is {DateTime.Now}"); },null,5000,1000);
+            
+            Task.Run(() =>
+            {
+                Console.WriteLine("Hi Dear!");
+                Thread.Sleep(10000);
+                t.Dispose();
+            });
+            
+
+            ThreadPool.QueueUserWorkItem(x => { Console.WriteLine($"{x} World"); },"Hello");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
