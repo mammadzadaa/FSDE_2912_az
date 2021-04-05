6d7832d06412319a276206c8f71d0905a57ec29b
mammadzadaa <mammedzadea@itstep.edu.az>
2021-04-05 18:02:59 +0400

    lesson_01_04_21

---
+++ System Programming/Lesson_01_04_21/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lesson_01_04_21
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static Mutex mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (Mutex.TryOpenExisting("SomeMutexName", out Mutex m))
+            {
+                MessageBox.Show("This app already open!");
+                Environment.Exit(0);
+            }
+            mutex = new Mutex(true, "SomeMutexName");
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ System Programming/Lesson_01_04_21_Async/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson_01_04_21_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int x = 0;
+            //Semaphore semaphore = new Semaphore(1, 1);
+            //Task[] tasks = new Task[2];
+            //for (int i = 0; i < tasks.Length; i++)
+            //{
+            //    tasks[i] = Task.Run(() =>
+            //    {
+            //        for (int j = 0; j < 1000000; j++)
+            //        {
+            //            semaphore.WaitOne();
+            //            x++;
+            //            semaphore.Release();
+            //        }
+            //    });
+            //}
+            //Task.WaitAll(tasks);
+            //Console.WriteLine(x);
+            Start();
+            Infinity();
+        }
+
+        static void Infinity()
+        {
+            int x = 0;
+            while (true)
+            {
+                Console.WriteLine(x++);
+                Thread.Sleep(500);
+            }
+        }
+
+        static async void  Start()
+        {
+            Console.WriteLine("Started");
+
+            // var result = GetResult();
+            // Console.WriteLine(result);
+
+            //GetResultThread();
+
+            // var task = GetResultTask();
+            // Console.WriteLine(task.Result);
+            int result = await GetResultTask();
+            Console.WriteLine(result);
+            Console.WriteLine("Finished");
+        }
+
+        static int GetResult()
+        {
+            Thread.Sleep(1000);
+            var result = 42;
+            return result;
+        }
+
+        static Task<int> GetResultTask()
+        {
+           return Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                var result = 42;
+                return result;
+            });
+            
+        }
+
+        static void GetResultThread()
+        {
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                var result = 42;
+                CallBack(result);
+            }).Start();
+        }
+        private static void CallBack(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
