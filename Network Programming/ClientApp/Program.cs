6b6de14be9ff52ad9c5f428fd6ae3fb36252df1c
mammadzadaa <mammedzadea@itstep.edu.az>
2021-04-13 21:55:16 +0400

    lesson_13_04_21

---
+++ Network Programming/ClientApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sender = new Socket(AddressFamily.InterNetwork,
+                                        SocketType.Stream,
+                                        ProtocolType.Tcp);
+
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5001);
+
+            sender.Connect(endPoint);
+            Console.WriteLine("I am connected!");
+            while (true)
+            {
+                var data = "Azeri torpaghinin hamimiz evladiyiq,\n Baki ezizi sheherim,\n Biz ona canla bagliyiq";
+                
+                sender.Send(Encoding.Unicode.GetBytes(data));
+                Console.WriteLine("Message been sent");
+                Thread.Sleep(2000);
+            }
+
+        }
+    }
+}
---
+++ Network Programming/Sockets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new Socket(  AddressFamily.InterNetwork,
+                                        SocketType.Stream,
+                                        ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Any,5001);
+            
+            listener.Bind(endPoint);
+
+            listener.Listen(10);
+
+            Console.WriteLine("Server is listening...");
+            var clientSocket = listener.Accept();
+            Console.WriteLine("Client Connected!");
+
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                clientSocket.Receive(buffer);
+
+                var msg = Encoding.Unicode.GetString(buffer);
+                Console.WriteLine(msg);
+            }
+
+        }
+    }
+}
