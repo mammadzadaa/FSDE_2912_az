4e3e0c43a12ade29e8c96f995aa423e45f7a8212
mammadzadaa <mammedzadea@itstep.edu.az>
2021-04-15 21:44:49 +0400

    lesson_15_04_21

---
+++ Network Programming/Lesson_15_04_21/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_15_04_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any,5001);
+
+            TcpListener server = new TcpListener(endPoint);
+            server.Start();
+            Console.WriteLine("Server is running...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client is connected...");
+
+                Task.Run(() =>
+                {
+                    using (var stream =  client.GetStream())
+                    {
+                       using (var reader = new StreamReader(stream))
+                       {                            
+                            while (true)
+                            {
+                                var msg = reader.ReadLine();
+                                if (msg == "exit")
+                                {
+                                    break;
+                                }
+                                Console.WriteLine(msg);
+                            }
+                       }
+
+                    }
+                
+
+
+                    /*byte[] buf = new byte[65536];
+                    //using (var stream = client.GetStream())
+                    //{
+                    //    while (true)
+                    //    {
+                    //        var size = stream.Read(buf,0,65536);
+                    //        var msg = Encoding.Unicode.GetString(buf,0,size);
+                    //        if (msg == "exit")
+                    //        {                               
+                    //            server.Stop();
+                    //            Console.WriteLine("Server disconnected!");
+                    //        }
+                    //        Console.WriteLine(msg);
+                    //    }
+                    }*/
+
+                });
+            }
+        }
+    }
+}
---
+++ Network Programming/Lesson_15_04_21_L/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lesson_15_04_21_L
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),5001);
+            TcpClient client = new TcpClient();
+            client.Connect(endPoint);
+
+            using (var writer = new StreamWriter(client.GetStream()))
+            {
+                while (true)
+                {
+                    var msg = Console.ReadLine();
+                    writer.WriteLine(msg);
+                    writer.Flush();
+                }
+            }
+
+        }
+    }
+}
