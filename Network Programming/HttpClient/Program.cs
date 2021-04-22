66c3c31a4509af37e27a142bed79f2f5534518f0
mammadzadaa <mammedzadea@itstep.edu.az>
2021-04-22 19:26:36 +0400

    Lesson_22_04_21

---
+++ Network Programming/HttpClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+
+            //HttpClient client = new HttpClient();
+            //HttpContent content = new StringContent("");
+            //client.PostAsync("",content);
+                        
+            listener.Prefixes.Add("http://localhost:8888/connection/");
+            
+            listener.Start();
+            Console.WriteLine("Witing for Connection...");
+            
+            HttpListenerContext context = listener.GetContext();
+            
+            HttpListenerRequest request = context.Request;
+            
+            HttpListenerResponse response = context.Response;
+            
+            string responseStr = $"<html><head><meta charset='utf8'></head><body><H1>Hello World!</H1><br><H2>{DateTime.Now}<H2></body></html>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+            
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            
+            output.Close();
+          
+            listener.Stop();
+            Console.WriteLine("Connection ended");
+        }
+    }
+}
---
+++ Network Programming/SMTP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     
+            MailAddress from = new MailAddress("somemail@gmail.com", "Agil");
+          
+            MailAddress to = new MailAddress("somemail@yandex.ru");
+            
+            MailMessage m = new MailMessage(from, to);
+            
+            m.Subject = "Test";
+            
+            m.Body = "<h2>Hey There, How are you?</h2>";
+           
+            m.IsBodyHtml = true;
+            
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            
+            smtp.Credentials = new NetworkCredential("somemail@gmail.com", "mypassword");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            Console.Read();
+        }
+    }
+}
